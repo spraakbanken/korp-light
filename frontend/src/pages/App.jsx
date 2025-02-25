05639ec95b413db7e6369f35a7f4f333353663fe
lisawad <lisawad@chalmers.com>
2025-02-25 17:56:08 +0100

    tested to create landing page

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,6 +1,7 @@
 // React Components
 import HelloKorpi from "../components/HelloKorpi/HelloKorpi.jsx";
 import NavigationBar from "../components/NavigationBar/NavigationBar.jsx";
+import LandingPage from "../components/LandingPage/LandningPage.jsx";
 
 //Bootstrap Components
 
@@ -11,8 +12,10 @@ import "./App.css"
 export default function App() {
     return (
         <>
-            <NavigationBar />
-            <HelloKorpi />
+           {/* <NavigationBar />
+            <HelloKorpi /> */}
+           < LandingPage />
+            
         </>
     );
 }
