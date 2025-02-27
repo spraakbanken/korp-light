af93f54385aabf3b84a879361caf86e868e7b0d8
pomesh <pomesh@student.chalmers.se>
2025-02-27 18:03:14 +0100

    started working on querybuilder

---
+++ frontend/src/services/queryParams.js
@@ -0,0 +1,22 @@
+let queryParams = {
+    "corpus" : String(),
+    "cqp" : String(),
+    "start": 0,
+    "end": 10,
+    "default_context": String(),
+    "context": [String()],
+    "show": [String()],
+    "show_struct": [String()],
+    "default_within": String(),
+    "within": [String()],
+    "in_order": Boolean,
+    "sort": String(),
+    "random_seed": Number(),
+    "cut": Number(),
+    "cqp#": String(),
+    "expand_prequeries": Boolean,
+    "incremental": Boolean,
+    "query_data": String(),
+}
+
+export default queryParams;
