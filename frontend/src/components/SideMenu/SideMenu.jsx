233e99f7f6e7f749159e3cca41e631eba0219765
Ida Åberg <idaaberg0310@gmail.com>
2025-03-01 12:59:45 +0100

    Update css and add tooltip to SideMenu

--- frontend/src/components/SideMenu/SideMenu.jsx
+++ frontend/src/components/SideMenu/SideMenu.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Offcanvas, Button, Nav } from "react-bootstrap";
+import { Offcanvas, Button, Nav, OverlayTrigger } from "react-bootstrap";
+import Tooltip from 'react-bootstrap/Tooltip';
 import "./SideMenu.css";
 
 export default function SideMenu() {
@@ -8,24 +9,32 @@ export default function SideMenu() {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    const menu_tip = (
+        <Tooltip id="menu_tooltip">
+            <strong>Meny</strong>
+        </Tooltip>
+    );
+
     return (
         <>
-            <Button variant="light" onClick={handleShow} className="menu-button">
-                ☰
-            </Button>
+            <OverlayTrigger placement="bottom" overlay={menu_tip}>
+                <Button variant="light" onClick={handleShow} className="menu-button">
+                    ☰
+                </Button>
+            </OverlayTrigger>
 
             <Offcanvas show={show} onHide={handleClose} className="side-menu">
                 <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>Menu</Offcanvas.Title>
+                    <Offcanvas.Title>Meny</Offcanvas.Title>
                 </Offcanvas.Header>
-                <Offcanvas.Body>
+                <Offcanvas.Body className="side-menu-body">
                     <Nav className="flex-column">
-                        <Nav.Link href="/">Hem</Nav.Link>
+                        <Nav.Link className="gray-row" href="/">Hem</Nav.Link>
                         {/* TODO link to help/tour */}
-                        <Nav.Link href="#">Användarhandledning</Nav.Link>
+                        <Nav.Link className="orange-row" href="#">Användarhandledning</Nav.Link>
                         {/* TODO link to history page */}
-                        <Nav.Link href="#">Historik</Nav.Link>
-                        <Nav.Link href="https://spraakbanken.gu.se/om">Mer om Språkbanken</Nav.Link>
+                        <Nav.Link className="gray-row" href="#">Historik</Nav.Link>
+                        <Nav.Link className="orange-row" href="https://spraakbanken.gu.se/om">Mer om Språkbanken</Nav.Link>
                     </Nav>
                 </Offcanvas.Body>
             </Offcanvas>
