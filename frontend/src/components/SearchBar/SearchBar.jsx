cf43cf253caa1774782bc34bb24008673d76d007
Ida Åberg <idaaberg0310@gmail.com>
2025-03-14 15:44:58 +0100

    Update searchbar with icon button

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,26 +1,32 @@
 import { useState } from "react";
 import "./SearchBar.css";
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import { Search } from "react-bootstrap-icons";
 
 //this gives type error for some reason! 
-export default function SearchBar({returnSearchInput}) {
+export default function SearchBar({ returnSearchInput }) {
 
     const [searchInput, setSearchInput] = useState("");
 
     return (
-        <>
-            <Form.Control
-                id="searchBar"
-                className="searchBar"
-                type="search"
-                placeholder="Search"
-                onChange={(e) => setSearchInput(e.target.value)}
-            />
-            <Button type="submit" onClick={
-                () => returnSearchInput(searchInput)}>
-            Search
-            </Button>
-        </>
+        <div className="searchBarContainer">
+            <div className="searchBarWrapper">
+                <Form.Control
+                    id="searchBar"
+                    className="searchBar"
+                    type="search"
+                    placeholder="Search"
+                    onChange={(e) => setSearchInput(e.target.value)}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                            returnSearchInput(searchInput);
+                        }
+                    }}
+                />
+                <button className="searchBarButton" type="button" onClick={() => returnSearchInput(searchInput)}>
+                    <Search />
+                </button>
+            </div>
+        </div>
     )
 }
