dcf303b178648ba361de6ab0d9735c307d0b6d85
pomesh <pomesh@student.chalmers.se>
2025-03-23 18:53:10 +0100

    got corpus list to show up, styling and logic next.

--- frontend/src/components/CorpusSelector/CorpusSelector.jsx
+++ frontend/src/components/CorpusSelector/CorpusSelector.jsx
@@ -1,32 +1,26 @@
 import { Accordion } from "react-bootstrap";
-
 import testdata from '../../services/testdata.json'
 
 function populateCorpus(e) {
     let title = null;
     let desc = null;
     let corpora = [];
-
-    console.log('e is', e)
-    console.log('TITLE: ', e[0])
-    console.log('DESCRIPTION', e[1].swe || e[1])
+    let subcorporaList = [];
       
     title = e[0]
-    desc = e[1].swe || e[1]
+    desc = e[1].swe || e[1] || ''
 
     if (e[2] !== undefined) {
         let arr = Object.keys(e[2])   
-        console.log('TEST SINGULAR CORPUS IS: ', arr)
-        console.log('TEST WITH CHILDNODES: ', arr.length)
         if (arr.length === 1) {
             corpora.push(arr.title)
         } else {
             Object.values(e[2]).forEach(corpus => {
             if (Array.isArray(corpus) ) {
-                console.log('CORPUS 1: ', corpus[0]) 
+                //console.log('CORPUS 1: ', corpus[0]) 
                 corpora.push(corpus[0])   
             } else {
-                console.log('CORPUS 2: ', corpus)
+                //console.log('CORPUS 2: ', corpus)
                 corpora.push(corpus)
             }
             })
@@ -34,18 +28,27 @@ function populateCorpus(e) {
     }
     
     if (e[3] !== undefined) {
-        console.log('\nSUBCORPORA')
-        Object.values(e[3]).forEach(() => {
-             singularElement => populateCorpus(singularElement);}
-        );
+        
+        Object.values(e[3]).forEach((el) => {
+            Object.values(el).forEach((el2) => {
+                subcorporaList.push(populateCorpus(el2));
+            })
+        });
     }
-
     return (
         <Accordion.Item eventKey={Math.random()}>
                 <Accordion.Header>{title}</Accordion.Header>
                 {desc ? <Accordion.Body>{desc}</Accordion.Body> : null}
-                <hr/>
-                {corpora.map(corpus => <Accordion.Body key={Math.random()}>{corpus}</Accordion.Body>)}
+                {corpora.map(corpus => 
+                    <Accordion.Body key={Math.random()}>
+                        {corpus}
+                    </Accordion.Body>)}
+                {subcorporaList.map(elem => 
+                    <Accordion.Body key={Math.random()}>
+                        <Accordion>
+                        {elem}
+                        </Accordion>
+                    </Accordion.Body>)}
         </Accordion.Item>
     );
   }
@@ -54,30 +57,9 @@ export default function CorpusSelector() {
     return (
         <>
             <Accordion>
-                
                 {Object.values(testdata).map((k) => {
                     return populateCorpus(k)
                 })}
-                <Accordion.Item eventKey="100">
-                    <Accordion.Header>Hello</Accordion.Header>
-                    <Accordion.Body>This field has more drop downs!</Accordion.Body>
-                    <Accordion.Body>
-                        <Accordion>
-                            <Accordion.Item eventKey="0">
-                                <Accordion.Header>Hello Again</Accordion.Header>
-                                <Accordion.Body>Description 2</Accordion.Body>
-                            </Accordion.Item>
-                            <Accordion.Item eventKey="1">
-                                <Accordion.Header>Hello Again</Accordion.Header>
-                                <Accordion.Body>Description 2</Accordion.Body>
-                            </Accordion.Item>
-                            <Accordion.Item eventKey="2">
-                                <Accordion.Header>Hello Again</Accordion.Header>
-                                <Accordion.Body>Description 2</Accordion.Body>
-                            </Accordion.Item>
-                        </Accordion>
-                    </Accordion.Body>
-                </Accordion.Item>
             </Accordion>
         </>
     );
