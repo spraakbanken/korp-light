473b0a0535e35d56ac05a04b5ca898b88b5400b6
Ida Åberg <idaaberg0310@gmail.com>
2025-03-26 11:18:05 +0100

    Changed query param to get 1000 results

--- frontend/src/services/queryParams.js
+++ frontend/src/services/queryParams.js
@@ -2,7 +2,7 @@ let queryParams = {
     "corpus" : String(),
     "cqp" : String(),
     "start": 0,
-    "end": 100,
+    "end": 1000,
     "default_context": String(),
     "context": [String()],
     "show": [String()],
