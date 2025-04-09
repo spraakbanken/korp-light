f763f0fc0078e526a8504436969ba06c4fd1656b
pomesh <pomesh@student.chalmers.se>
2025-04-09 10:44:54 +0200

    Added some categories to advancedsearch.

--- frontend/src/components/AdvancedSearch/AdvancedSearch.jsx
+++ frontend/src/components/AdvancedSearch/AdvancedSearch.jsx
@@ -1,14 +1,52 @@
 import './AdvancedSearch.css'
 
+import Accordion from 'react-bootstrap/Accordion';
+
 export default function AdvancedSearch({words}) {
+    
+    function generateEntry(word, idx) {
+        if (word !== "") {
+            
+            return (
+            <Accordion>
+                <Accordion.Item eventKey={idx}>    
+                    <Accordion.Header key={idx}>
+                        {word}
+                    </Accordion.Header>
+                    <Accordion.Body>
+                        Grundform
+                    </Accordion.Body>
+                    <Accordion.Body>
+                        Ord
+                    </Accordion.Body>
+                    <Accordion.Body>
+                        <Accordion>
+                            <Accordion.Header>
+                            Ordklass
+                            </Accordion.Header>
+                            <Accordion.Body>
+                                Adjektiv
+                            </Accordion.Body>
+                            <Accordion.Body>
+                                Substantiv
+                            </Accordion.Body>
+                            <Accordion.Body>
+                                Verb
+                            </Accordion.Body>
+                        </Accordion>
+                    </Accordion.Body>
+                </Accordion.Item>     
+            </Accordion>  
+            );        
+        }
+    
+    }
+    
     return(
         <div className='advanced__search__container'>
+            
             {Object.values(words).map((word, idx) => {
-            
-            if (word !== "")
-                return <p className='advanced__search__word'
-                    key={idx}>{word}</p>
-            
+                return generateEntry(word, idx);
             })}
         </div>
     );
