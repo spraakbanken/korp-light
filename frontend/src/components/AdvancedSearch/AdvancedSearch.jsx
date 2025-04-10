f4d18fb8837bcdf5665906994e625ae48379c6ee
pomesh <pomesh@student.chalmers.se>
2025-04-10 14:51:32 +0200

    Added word classes to advanced search.

--- frontend/src/components/AdvancedSearch/AdvancedSearch.jsx
+++ frontend/src/components/AdvancedSearch/AdvancedSearch.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useActionState, useEffect, useState } from 'react';
 import './AdvancedSearch.css'
 
 import AdvancedSearchEntry from './AdvancedSearchEntry.jsx';
@@ -7,6 +7,7 @@ import Dropdown from 'react-bootstrap/Dropdown';
 
 export default function AdvancedSearch({words, returnWordsDict}) {
     
+    const [wordStates, setWordStates] = useState([]);
     const [wordsDict, setWordsDict] = useState({});
 
     function handleClick(word, tag) {
@@ -19,11 +20,31 @@ export default function AdvancedSearch({words, returnWordsDict}) {
         returnWordsDict(wordsDict);
     }, [wordsDict, returnWordsDict])
     
+    const createComponent = (entryName) => {
+        console.log('creating component ', entryName)
+        setWordStates([...wordStates, <p className='advanced__search__word'>{entryName}</p>])
+    } 
+
+
     return(
         <div className='advanced__search__container'>
             {Object.values(words).map((word, idx) => {
-               return <AdvancedSearchEntry word={word} idx={idx} returnWordTag={(tag) => {handleClick(word, tag)}}/>;
+               return <AdvancedSearchEntry word={word} idx={idx} returnWordTag={(tag) => {handleClick(word, tag)}}/>
             })}
+
+            {Object.values(wordStates).map((word, idx) => {
+                return word;
+            })}
+
+            <div>
+                    <Dropdown>
+                        <Dropdown.Toggle className='advanced__search__append'>+</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick={() => createComponent('Adverb')}>Adverb</Dropdown.Item>
+                            <Dropdown.Item onClick={() => createComponent('Substantiv')}>Substantiv</Dropdown.Item>
+                        </Dropdown.Menu>
+                </Dropdown>
+              </div>
         </div>
     );
 }
