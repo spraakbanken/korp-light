bec5d14904bda21dc6cad6cbbd1acd1b3852e618
pomesh <pomesh@student.chalmers.se>
2025-04-11 07:50:27 +0200

    changed base for deployed page

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/korpi/',
+  base: '/',
   plugins: [react()],
   server: {
     watch: {
       usePolling: true,
     }
-  }
+  },
 })
