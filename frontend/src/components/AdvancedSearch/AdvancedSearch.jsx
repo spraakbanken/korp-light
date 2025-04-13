e2222a5420ce501c50302f1335f0493ecbdbd993
pomesh <pomesh@student.chalmers.se>
2025-04-14 00:58:15 +0200

    more bug fixes in adv search. dragging still doesnt work.

--- frontend/src/components/AdvancedSearch/AdvancedSearch.jsx
+++ frontend/src/components/AdvancedSearch/AdvancedSearch.jsx
@@ -1,19 +1,20 @@
 import { useEffect, useState } from 'react';
-import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
+import { horizontalListSortingStrategy, SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
 import './AdvancedSearch.css'
 
 import AdvancedSearchEntry from './AdvancedSearchEntry.jsx';
 
 import Dropdown from 'react-bootstrap/Dropdown';
-import { DndContext } from '@dnd-kit/core';
+import { closestCorners, DndContext } from '@dnd-kit/core';
 
 export default function AdvancedSearch({words, returnWordsDict}) {
     
-    const [wordElements, setWordElements] = useState([]);
+    const [wordElements, setWordElements] = useState([{
+        id: 1, wordEntry: ""
+    }]);
     const [wordsDict, setWordsDict] = useState({});
 
     function handleClick(word, tag) {
-        
         setWordsDict({...wordsDict, [word]: tag})
     }
 
@@ -22,26 +23,35 @@ export default function AdvancedSearch({words, returnWordsDict}) {
     }, [wordsDict, returnWordsDict])
     
     const createComponent = (entryName) => {
-        setWordElements([...wordElements, <AdvancedSearchEntry word={entryName} idx={0} returnWordTag={(tag) => {handleClick(entryName, tag)}}/>])
+        setWordElements([...wordElements, {id: wordElements.length+1, 
+            wordEntry: entryName}])
     } 
 
     useEffect(() => {
+        console.log('words are', words);
         let lastWord = words[words.length - 1]; 
-        console.log('lastWord', lastWord);
-        
-        setWordElements([...wordElements, 
-            <AdvancedSearchEntry word={lastWord} idx={0} returnWordTag={(tag) => {handleClick(lastWord, tag)}}/>]);
-
+        setWordElements((prev) => [...prev, {id: prev.length+1, 
+            wordEntry: lastWord}]);
     }, [words]);
 
+    const onDragStart = (e) => {
+        console.log('onDragStart', e);
+    }
+
     return(
-        <>
-        <DndContext>
         <div className='advanced__search__container'>
-            <SortableContext items={wordElements} strategy={verticalListSortingStrategy}>
-            {wordElements.map(w => w)}
+            <DndContext collisionDetection={closestCorners} onDragEnd={onDragStart}>
+            <SortableContext items={wordElements} strategy={horizontalListSortingStrategy}>
+            {wordElements.map((w) => {
+                if (w.wordEntry) {
+                    return <AdvancedSearchEntry key={w.id} word={w.wordEntry} idx={w.id} 
+                        returnWordTag={(tag) => {handleClick(w.wordEntry, tag)}}/>
+                }
+            })}
+            </SortableContext>
+            </DndContext>
             <div>
-                    <Dropdown>
+                    <Dropdown key={99999}>
                         <Dropdown.Toggle className='advanced__search__append'>+</Dropdown.Toggle>
                         <Dropdown.Menu>
                             <Dropdown.Item onClick={() => createComponent('Adverb')}>Adverb</Dropdown.Item>
@@ -49,9 +59,7 @@ export default function AdvancedSearch({words, returnWordsDict}) {
                         </Dropdown.Menu>
                 </Dropdown>
               </div>
-        </SortableContext>
+        
         </div>
-        </DndContext>
-        </>
     );
 }
--- frontend/src/components/AdvancedSearch/AdvancedSearchEntry.jsx
+++ frontend/src/components/AdvancedSearch/AdvancedSearchEntry.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './AdvancedSearch.css'
 
 import { useSortable } from '@dnd-kit/sortable';
@@ -19,8 +19,8 @@ export default function AdvancedSearchEntry({word, idx, returnWordTag}) {
     };
 
     function handleClick(word, e) {
-        console.log("word is ", word);
         const targetText = e.target.text
+        console.log('attributes', listeners);
 
         if (targetText === 'Grundform') {
             setShowGrundform(true);
@@ -32,10 +32,10 @@ export default function AdvancedSearchEntry({word, idx, returnWordTag}) {
     }
 
     function generateEntry(word, idx) {
-        console.log('word ', word)
-        if (word !== "") {
+        if (word !== undefined) {
             return (
-                <div ref={setNodeRef} style={style} {...attributes} {...listeners} 
+                <div ref={setNodeRef} {...attributes} {...listeners}
+                    style={style} 
                     className='advanced__search__entry'>
                     <Dropdown key={idx}>
                         <Dropdown.Toggle className='advanced__search__word'>
@@ -45,9 +45,6 @@ export default function AdvancedSearchEntry({word, idx, returnWordTag}) {
                         <Dropdown.Menu>
                         <Dropdown.Item onClick={(e) => {handleClick(word, e); returnWordTag('Grundform');}}>Grundform</Dropdown.Item>
                         <Dropdown.Item onClick={(e) => {handleClick(word, e); returnWordTag('Ord');}}>Ord</Dropdown.Item>
-                        <Dropdown.Divider />
-                        <Dropdown.Item href="#/action-3">Substantiv</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">Verb</Dropdown.Item>
                         </Dropdown.Menu>
                 </Dropdown>
                 {showOrdform && <p className='advanced__search__small__icon'>O</p>}
