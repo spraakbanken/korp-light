5b3066f8eee6079c5ee4994665aa1b9dd53e20be
Ida Åberg <idaaberg0310@gmail.com>
2025-04-22 14:24:27 +0200

    added variable to display percentage of loaded words to progressbar

--- frontend/src/components/ProgressBar/ProgressBar.jsx
+++ frontend/src/components/ProgressBar/ProgressBar.jsx
@@ -1,9 +1,10 @@
 import { ProgressBar } from 'react-bootstrap';
 import { CheckAll } from 'react-bootstrap-icons';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, use } from 'react';
 import "./ProgressBar.css";
+import { Timer } from 'lucide-react';
 
-export default function LoadingProgressBar({ isLoading }) {
+export default function LoadingProgressBar({ isLoading, percentLoaded}) {
     const [isComplete, setIsComplete] = useState(false);
     const [hasSearched, setHasSearched] = useState(false); 
 
@@ -25,7 +26,7 @@ export default function LoadingProgressBar({ isLoading }) {
             {isLoading && hasSearched && (
                 <div className="progress-bar-container">
                     <div className="progress-bar">
-                        <ProgressBar animated now={100} />
+                        <ProgressBar animated now={percentLoaded} />
                     </div>
                 </div>
             )}
@@ -33,7 +34,7 @@ export default function LoadingProgressBar({ isLoading }) {
             {!isLoading && isComplete && (
                 <div className='progress-finished'>
                     <div className="progress-bar">
-                        <ProgressBar animated now={100} />
+                        <ProgressBar animated now={percentLoaded} />
                     </div>
                     <div className="check-all-icon">
                         <CheckAll size={24} />
