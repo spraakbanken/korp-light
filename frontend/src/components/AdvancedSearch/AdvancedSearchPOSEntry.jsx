2330dde06f65897dacd3605c13436e21003df957
pomesh <pomesh@student.chalmers.se>
2025-04-22 14:03:39 +0200

    pos entries now have colours defined

--- frontend/src/components/AdvancedSearch/AdvancedSearchPOSEntry.jsx
+++ frontend/src/components/AdvancedSearch/AdvancedSearchPOSEntry.jsx
@@ -5,17 +5,19 @@ import { Button } from 'react-bootstrap';
 
 import pos_list from '../../services/part-of-speech-list'
 
-export default function AdvancedSearchPOSEntry({word, idx, handleDelete, handleChevronClick}) {
+export default function AdvancedSearchPOSEntry({word, idx, handleDelete, handleChevronClick, advButtonColour}) {
 
 
-
+    const style = {
+        "--adv-button-bgc": advButtonColour,
+    };
 
     return (
     <div className='advanced__search__entry'>
         <div className='advanced__search__entry__wrapper'>
-            <Dropdown key={idx} className='advanced__search__word'>
+            <Dropdown key={idx} className='advanced__search__word_pos' style={style}>
                     <Button onClick={(e) => handleChevronClick(idx, -1)} className='advanced__search__chevron'>{'<'}</Button>
-                        <Dropdown.Toggle className='advanced__search__word'>
+                        <Dropdown.Toggle className='advanced__search__word_pos'>
                             {word}
                         </Dropdown.Toggle>
                     <Button onClick={(e) => handleChevronClick(idx, 1)} className='advanced__search__chevron'>{'>'}</Button>
