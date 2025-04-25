7ae93fea41f5d7877814c12bd4a7ca1d1d1c9049
pomesh <pomesh@student.chalmers.se>
2025-04-25 11:24:42 +0200

    updated for new build

--- frontend/src/components/InfoText/Date.jsx
+++ frontend/src/components/InfoText/Date.jsx
@@ -5,7 +5,7 @@ export default function Date() {
     return (
         <div className="dateContainer">
             <div className="dateWrapper">
-                <p>Last updated: 2025-04-16 11:16</p>
+                <p>Last updated: 2025-04-25 11:25</p>
             </div>
         </div>
     )
